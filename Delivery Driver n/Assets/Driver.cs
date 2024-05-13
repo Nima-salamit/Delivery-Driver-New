8c93d6d4a92f6ba4b725ae50026b785f0da7e3ab
Nima Sl <110297816+Nima-salamit@users.noreply.github.com>
2024-05-13 10:47:22 +0330

    Last commit

--- Delivery Driver n/Assets/Driver.cs
+++ Delivery Driver n/Assets/Driver.cs
@@ -1,17 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] private float steerSpeed = 0.2f;
-
-    [SerializeField] private float moveSpeed = 0.01f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
 
     // Update is called once per frame
     void Update()
@@ -20,5 +17,17 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }     
     }
 }
